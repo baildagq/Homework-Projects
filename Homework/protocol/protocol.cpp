d376803b94a0d3b01bf5ee57a66912a6f254637a
齐强 <2017011436@secoder.net>
2019-11-25 16:07:25 +0800

    problem4 protocol

--- Homework/protocol/protocol.cpp
+++ Homework/protocol/protocol.cpp
@@ -1,6 +1,8 @@
 #include "rip.h"
 #include <stdint.h>
 #include <stdlib.h>
+#include <cstdio>
+#include <arpa/inet.h>
 
 /*
   在头文件 rip.h 中定义了如下的结构体：
@@ -43,9 +45,70 @@
  * Metric 转换成小端序后是否在 [1,16] 的区间内，
  * Mask 的二进制是不是连续的 1 与连续的 0 组成等等。
  */
+
+
+// IP 报头的长度根据 header Length 计算得到
+// UDP 报头根据观察和简单查询假定为8个字节
 bool disassemble(const uint8_t *packet, uint32_t len, RipPacket *output) {
-  // TODO:
-  return false;
+    // TODO:
+    uint32_t packetLen = ((uint32_t)packet[2] << 8) | ((uint32_t)packet[3]);  
+    uint8_t headerLen = (packet[0] & 0xf) * 4; // 取出headerLen后，因为单位为4byte, 再乘4
+    uint8_t udpLen = 8;
+
+    const uint8_t *udpPacket = packet + headerLen; 
+    const uint8_t *ripPacket = udpPacket + udpLen; 
+
+    // for debug
+    // check whether len < Total Length
+    if (len < packetLen)
+        return false;
+    // check whether command == 1 or command == 2 
+    if (ripPacket[0] != 1 && ripPacket[0] != 2)
+        return false;
+    // check whether Version == 2 
+    if (ripPacket[1] != 2)
+        return false;
+    // check whether Zero == 0
+    if (ripPacket[2] != 0 || ripPacket[3] != 0)
+        return false;
+
+    // begin construct output
+    output->numEntries = (packetLen - headerLen - udpLen) / 20; // update number of Entry
+    output->command = ripPacket[0]; // update command
+    for (int i = 0;i < output->numEntries;i ++) {
+        const uint8_t *ripEntry = ripPacket + 4 + 20 * i;
+        uint16_t family = ((uint16_t)ripEntry[0] << 8) | (uint16_t)ripEntry[1];
+        // check whether (command == 1 && family == 0)  || (command == 2 && family == 2)
+        if (!((output->command == 1 && family == 0) || (output->command == 2 && family == 2)))
+            return false;
+        // check whether tag = 0;
+        uint16_t tag = ((uint16_t)ripEntry[2] << 8) | (uint16_t)ripEntry[3];
+        if (tag != 0)
+            return false;
+        // check whether metric belong [0, 16]
+        uint32_t metric = ripEntry[16] << 24 | ripEntry[17] << 16 | ripEntry[18] << 8 | ripEntry[19];
+        if (!(metric >= 1 && metric <= 16))
+            return false;
+        // check mask format
+        uint32_t mask = ripEntry[8] << 24 | ripEntry[9] << 16 | ripEntry[10] << 8 | ripEntry[11];
+        bool appear1 = false;
+        for (int j = 0;j < 32; j++) {
+            if (mask % 2 == 1) {
+                appear1 = true;
+            } else {
+                if (appear1){
+                    return false;
+                }
+            }
+            mask >>= 1;
+        }
+        // pass all check, begin record
+        output->entries[i].addr = ripEntry[7] << 24 | ripEntry[6] << 16 | ripEntry[5] << 8 | ripEntry[4]; 
+        output->entries[i].mask = ripEntry[11] << 24 | ripEntry[10] << 16 | ripEntry[9] << 8 | ripEntry[8];
+        output->entries[i].nexthop = ripEntry[15] << 24 | ripEntry[14] << 16 | ripEntry[13] << 8 | ripEntry[12]; 
+        output->entries[i].metric = ripEntry[19] << 24 | ripEntry[18] << 16 | ripEntry[17] << 8 | ripEntry[15];
+    }
+    return true;
 }
 
 /**
@@ -60,5 +123,43 @@ bool disassemble(const uint8_t *packet, uint32_t len, RipPacket *output) {
  */
 uint32_t assemble(const RipPacket *rip, uint8_t *buffer) {
   // TODO:
-  return 0;
+  // update rip head
+  buffer[0] = rip->command;
+  buffer[1] = 0x2;
+  buffer[2] = 0x0;
+  buffer[3] = 0x0;
+  for (int i = 0;i < rip->numEntries; i ++) {
+      // update family
+      buffer[4 + 20 * i] = 0;
+      if (rip->command == 1)
+          // command = 1, family = 0
+          buffer[4 + 20 * i + 1] = 0;
+      else 
+          // command = 2, family = 2
+          buffer[4 + 20 * i + 1] = 2;
+      // update tag
+      buffer[4 + 20 * i + 2] = 0;
+      buffer[4 + 20 * i + 3] = 0;
+      // update addr
+      buffer[4 + 20 * i + 7] = (rip->entries[i].addr & 0xff000000) >> 24;
+      buffer[4 + 20 * i + 6] = (rip->entries[i].addr & 0x00ff0000) >> 16;
+      buffer[4 + 20 * i + 5] = (rip->entries[i].addr & 0x0000ff00) >> 8;
+      buffer[4 + 20 * i + 4] = (rip->entries[i].addr & 0x000000ff);
+      // update mask
+      buffer[4 + 20 * i + 11] = (rip->entries[i].mask & 0xff000000) >> 24;
+      buffer[4 + 20 * i + 10] = (rip->entries[i].mask & 0x00ff0000) >> 16;
+      buffer[4 + 20 * i + 9]  = (rip->entries[i].mask & 0x0000ff00) >> 8;
+      buffer[4 + 20 * i + 8]  = (rip->entries[i].mask & 0x000000ff);
+      // update nexthop
+      buffer[4 + 20 * i + 15] = (rip->entries[i].nexthop & 0xff000000) >> 24;
+      buffer[4 + 20 * i + 14] = (rip->entries[i].nexthop & 0x00ff0000) >> 16;
+      buffer[4 + 20 * i + 13] = (rip->entries[i].nexthop & 0x0000ff00) >> 8;
+      buffer[4 + 20 * i + 12] = (rip->entries[i].nexthop & 0x000000ff);
+      // update metric
+      buffer[4 + 20 * i + 19] = (rip->entries[i].metric & 0xff000000) >> 24;
+      buffer[4 + 20 * i + 18] = (rip->entries[i].metric & 0x00ff0000) >> 16;
+      buffer[4 + 20 * i + 17] = (rip->entries[i].metric & 0x0000ff00) >> 8;
+      buffer[4 + 20 * i + 16] = (rip->entries[i].metric & 0x000000ff);
+  }
+  return 4 + 20 * rip->numEntries;
 }
